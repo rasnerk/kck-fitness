1bd8db7223be80304a16c57434204ab31134146b
Keifer Rasner <rasnerkeifer@gmail.com>
2022-03-03 11:52:30 -0600

    Wireframing for 'About Us' & 'Contact Us', background design changed in all sections

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,7 +1,21 @@
+import './about.css';
+
 function About() {
     return (
         <div className="about">
-            About
+            <div className="logo-about">
+                <img src='logotest.png' alt='KCK Logo'></img>
+            </div>
+            <div className='about-content'>
+                <div className='sec-1'>
+                    <div className='im'>
+                        PICTURE
+                    </div>
+                    <div className='text'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                </div>
+            </div>
         </div>
     );
   }
--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,7 +1,16 @@
+import './contact.css';
+
 function Contact() {
     return (
         <div className="contact">
-            Contact
+            <div className='sec-1'>
+                <div></div>
+                <div></div>
+            </div>
+            <div className='sec-1'>
+                <div></div>
+                <div></div>
+            </div>
         </div>
     );
   }
