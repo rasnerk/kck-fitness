77e6a172a0121551326da066596bbf0f21f71cb6
Keifer Rasner <rasnerkeifer@gmail.com>
2022-06-22 12:00:08 -0500

    added images for matt

--- src/lib/testimonials.js
+++ src/lib/testimonials.js
@@ -1,5 +1,5 @@
 export const kckTestimonials = [
-    { client: "Matt", message: "I always struggled trying to gain muscle mass. After working with the guys at kck I saw major changes in a matter of a couple weeks. I would recommend anyone who is struggling to meet their fitness goals to talk to these guys.", before: "imgs/keifer-before.jpg", after: "imgs/keifer-after.jpg" },
+    { client: "Matt", message: "I always struggled trying to gain muscle mass. After working with the guys at kck I saw major changes in a matter of a couple weeks. I would recommend anyone who is struggling to meet their fitness goals to talk to these guys.", before: "imgs/matt-before.jpg", after: "imgs/matt-after.jpg" },
     { client: "Keifer", message: "Check out the results of Keifer's fitness show!", before: "imgs/keifer-before.jpg", after: "imgs/keifer-after.jpg" },
     { client: "Nathan", message: "Worked with Keifer for a couple of years, and he really helped me take slow steps in the right direction. After it is all said and done, I've lost well over 100 pounds.", before: "imgs/nathan-before.jpg", after: "imgs/nathan-after.jpg" }
 ]
